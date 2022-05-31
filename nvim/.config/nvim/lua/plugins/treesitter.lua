b8fdb1283e18162c7c1e9684e1b99ae7d6744330
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-05-31 22:31:15 +1000

    Formatting.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 -- Configuration for Treesitter
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {
-        "python", 
-        "r", 
-        "latex", 
-        "vim",
-        "lua", 
-        "bash", 
-        "regex", 
-        "json",
-        "javascript",
-        "html"
-    },
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"python",
+		"r",
+		"latex",
+		"vim",
+		"lua",
+		"bash",
+		"regex",
+		"json",
+		"javascript",
+		"html"
+	},
 
-    -- Syntax highlighting
-    highlight = {
-        enable = true,
-        disable = {},
-        additional_vim_regex_highlighting = false,
-    },
+	-- Syntax highlighting
+	highlight = {
+		enable = true,
+		disable = {},
+		additional_vim_regex_highlighting = false,
+	},
 }
