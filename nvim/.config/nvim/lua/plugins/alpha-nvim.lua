1d966be0fba1e04ad5e24387ea243c6b279f9f89
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-06-02 11:20:29 +1000

    Started configuring alpha-nvim.

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -1,3 +1,28 @@
-require('alpha').setup(
-	require('alpha.themes.dashboard').config
-)
+local alpha = require('alpha')
+
+local dashboard = require('alpha.themes.dashboard')
+
+dashboard.section.buttons.val = {
+  dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+  dashboard.button('SPC f f', '  Find file', '<cmd>Telescope find_files<CR>'),
+  dashboard.button('SPC f g', '  Live grep', '<cmd>Telescope live_grep<CR>'),
+  dashboard.button('SPC f d', '  Open personal dotfiles', '<cmd>cd ~/.config/nvim<CR><cmd>NvimTreeOpen<CR>'),
+  dashboard.button('SPC c', '   Select colourscheme', '<cmd>Telescope colorscheme<CR>'),
+  dashboard.button('q', '⎋  Quit', '<cmd>qa<CR>')
+}
+
+alpha.setup(dashboard.config)
+
+--   New file                                      e
+
+--   Find file                               SPC f f
+
+--   Recently opened files                   SPC f h
+
+--   Frecency/MRU                            SPC f r
+
+--   Find word                               SPC f g
+
+--   Jump to bookmarks                       SPC f m
+
+--   Open last session                       SPC s l
