ad61840348e9d2c67e55f0c3223d103fce89957e
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-06-07 20:00:02 +1000

    Changed some icons.

--- nvim/.config/nvim/lua/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -10,29 +10,17 @@ end
 
 -- print(packer_plugins)
 
+-- For more icons, head to https://www.nerdfonts.com/cheat-sheet
+
 dashboard.section.buttons.val = {
   dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
   dashboard.button('SPC f f', '  Find file', '<cmd>Telescope find_files<CR>'),
   dashboard.button('SPC f g', '  Live grep', '<cmd>Telescope live_grep<CR>'),
-  dashboard.button('SPC c', '   Select colourscheme', '<cmd>Telescope colorscheme<CR>'),
+  dashboard.button('SPC c', '  Select colourscheme', '<cmd>Telescope colorscheme<CR>'),
 	dashboard.button('SPC f d', '  Configure', '<cmd>cd ~/.config/nvim<CR><cmd>e init.lua<CR>'),
-  dashboard.button('q', '⎋  Quit', '<cmd>qa<CR>')
+  dashboard.button('q', '  Quit', '<cmd>qa<CR>')
 }
 
 dashboard.section.footer.val = footer()
 
 alpha.setup(dashboard.config)
-
---   New file                                      e
-
---   Find file                               SPC f f
-
---   Recently opened files                   SPC f h
-
---   Frecency/MRU                            SPC f r
-
---   Find word                               SPC f g
-
---   Jump to bookmarks                       SPC f m
-
---   Open last session                       SPC s l
