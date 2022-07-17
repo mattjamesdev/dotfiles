7695c509f9671e01161314414483342e980f8c13
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-07-17 11:27:26 +1000

    Allow transparent background with Gruvbox.

--- nvim/.config/nvim/lua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -1,2 +1,10 @@
+-- Config for gruvbox
+require('gruvbox').setup({
+  overrides = {
+    Normal = { bg = "none" },
+  }
+})
+
+-- Apply the colour scheme
 vim.o.background = 'dark'
 vim.cmd('colorscheme gruvbox')
