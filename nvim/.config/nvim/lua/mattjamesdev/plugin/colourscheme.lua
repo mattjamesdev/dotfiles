d9b9fcebd6c742f6ca74902472a469e1548c2da1
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-09-10 15:39:33 +1000

    Added extra config options for colourschemes.

--- nvim/.config/nvim/lua/mattjamesdev/plugin/colourscheme.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugin/colourscheme.lua
@@ -1,10 +1,20 @@
--- Config for gruvbox
+-- Config for some colourschemes
+-- Gruvbox
 require('gruvbox').setup({
   -- overrides = {
   --   Normal = { bg = "none" },
   -- }
 })
 
+-- Tundra
+require('nvim-tundra').setup({
+  transparent_background = false,
+})
+
 -- Apply the colour scheme
-vim.o.background = 'dark'
-vim.cmd('colorscheme gruvbox')
+vim.opt.background = 'dark'
+
+-- vim.cmd('colorscheme coal')
+-- vim.cmd('colorscheme gruvbox')
+-- vim.cmd('colorscheme tundra')
+-- vim.cmd('colorscheme vscode')
