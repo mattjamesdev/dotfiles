2fc2ccb698c26fa71ef52eab47e0e288e35b4114
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-09-13 21:43:41 +1000

    Customised Telescope a bit.

--- nvim/.config/nvim/lua/mattjamesdev/plugin/telescope.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugin/telescope.lua
@@ -1,5 +1,17 @@
 require('telescope').setup({
   defaults = {
-    file_ignore_patterns = { "node_modules", "__pycache__", ".DS_Store" }
+    file_ignore_patterns = {
+      "node_modules",
+      "__pycache__",
+      ".DS_Store",
+      ".venv",
+      "venv",
+    },
+    layout_config = {
+      prompt_position = "top",
+    },
+    sorting_strategy = "ascending",
+    prompt_prefix = "   ",
+    selection_caret = "  "
   }
 })
