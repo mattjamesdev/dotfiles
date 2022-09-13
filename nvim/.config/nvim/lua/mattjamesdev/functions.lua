0aade98bf07f94a3ec65fda86dc76979fd2d3cd7
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-09-13 20:51:12 +1000

    misc: formatting

--- nvim/.config/nvim/lua/mattjamesdev/functions.lua
+++ nvim/.config/nvim/lua/mattjamesdev/functions.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- Smart dd: does not yank if the line is only whitespace or empty
 -- (u/Alleyria's comment in https://www.reddit.com/r/neovim/comments/w0jzzv/smart_dd/)
-function M.smart_dd()
+M.smart_dd = function()
   if vim.api.nvim_get_current_line():match("^%s*$") then
     return '"_dd'
   else
@@ -12,7 +12,7 @@ end
 
 -- Smart visual d: does not yank if selection is only whitespace or empty
 -- (u/mknurs's comment in https://www.reddit.com/r/neovim/comments/w0jzzv/smart_dd/)
-function M.smart_visual_d()
+M.smart_visual_d = function()
   local l, c = unpack(vim.api.nvim_win_get_cursor(0))
   for _, line in ipairs(vim.api.nvim_buf_get_lines(0, l-1, l, true)) do
     if line:match("^%s*$") then
