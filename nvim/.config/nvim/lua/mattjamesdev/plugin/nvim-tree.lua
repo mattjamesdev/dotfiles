24b5a0f9dbdbfd7992dc6022b4521ac092699425
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-09-15 22:18:04 +1000

    Tweaked nvim-tree a bit.

--- nvim/.config/nvim/lua/mattjamesdev/plugin/nvim-tree.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugin/nvim-tree.lua
@@ -1,6 +1,18 @@
 require('nvim-tree').setup({
-    -- auto_close = true,
-    view = {
-        width = 40,
+  -- auto_close = true,
+  hijack_cursor = true,
+  view = {
+    width = 40,
+  },
+  renderer = {
+    highlight_git = true,
+    indent_markers = {
+      enable = true,
     },
+    icons = {
+      git_placement = "after",
+    }
+  }
 })
+
+vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { link = "WinSeparator" })
