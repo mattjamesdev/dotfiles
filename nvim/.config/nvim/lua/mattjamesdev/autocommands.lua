4ccfbd85e37af10147c036e21dfd9bf80756faa4
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-09-22 00:08:06 +1000

    Added highlight on yank (and misc formatting stuff).

--- nvim/.config/nvim/lua/mattjamesdev/autocommands.lua
+++ nvim/.config/nvim/lua/mattjamesdev/autocommands.lua
@@ -1,22 +1,44 @@
+-- Autocommands
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+
 -- Enable hlsearch when searching, then disable when entering Insert mode
-local incsearch_highlight = vim.api.nvim_create_augroup('vimrc-incsearch-highlight', {})
-vim.api.nvim_create_autocmd({ 'CmdlineEnter' }, {
+local incsearch_highlight = augroup('vimrc-incsearch-highlight', {})
+
+autocmd({ 'CmdlineEnter' }, {
   pattern = { '/,\\?' },
   command = 'set hlsearch',
   group = incsearch_highlight
 })
-vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+autocmd({ 'InsertEnter' }, {
   pattern = '*',
   command = 'set nohlsearch',
   group = incsearch_highlight
 })
 
+
 -- Automatically close nvim-tree if it is the last buffer remaining
-vim.api.nvim_create_autocmd("BufEnter", {
+autocmd("BufEnter", {
   nested = true,
   callback = function()
     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
       vim.cmd("quit")
     end
   end
+})
+
+
+-- Highlight yanked text
+local yank_group = augroup("HighlightYank", {})
+
+autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
