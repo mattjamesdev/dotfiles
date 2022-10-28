5effb999c0d08b15169d6f23d8eab7bf7bb3cdb9
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-10-29 10:25:48 +1100

    Customised the look of completion windows.

--- nvim/.config/nvim/lua/mattjamesdev/plugin/lsp/cmp.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugin/lsp/cmp.lua
@@ -66,8 +66,16 @@ cmp.setup({
   -- Get VSCode-style pictograms with lspkind-nvim
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 40, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = "...",
+      menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
+      }),
 
       -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
