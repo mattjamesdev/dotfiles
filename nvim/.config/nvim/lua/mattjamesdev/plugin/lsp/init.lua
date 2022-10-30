294de28845051dbd9935e55d34dd6d395489bd81
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2022-10-30 23:31:26 +1100

    Changed code actions keymap to avoid mixing with ccc.nvim's keymap.

--- nvim/.config/nvim/lua/mattjamesdev/plugin/lsp/init.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugin/lsp/init.lua
@@ -22,7 +22,7 @@ local on_attach = function(client, bufnr)
   end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, {
+  vim.keymap.set('n', '<space>lc', vim.lsp.buf.code_action, {
     desc = 'Code actions', unpack(bufopts)
   })
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
