e8913d35620f13339825d3e185ebadc6d3057a26
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2023-04-18 23:15:28 +1000

    Added descriptions to some keymaps and prefixes.

--- nvim/.config/nvim/lua/mattjamesdev/plugins/lsp/server_config.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugins/lsp/server_config.lua
@@ -6,25 +6,47 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {
+    desc = "Go to declaration", unpack(bufopts)
+  })
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {
     desc = 'Go to definition',
     unpack(bufopts)
   })
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {
+    desc = "Hover", unpack(bufopts)
+  })
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<space>k', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>k', vim.lsp.buf.signature_help, {
+    desc = "Signature help", unpack(bufopts)
+  })
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, {
+    desc = "Add workspace folder", unpack(bufopts)
+  })
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, {
+    desc = "Remove workspace folder", unpack(bufopts)
+  })
   vim.keymap.set('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  end, {
+    desc = 'List workspace folders',
+    unpack(bufopts)
+  })
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {
+    desc = "Type definition",
+    unpack(bufopts)
+  })
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {
+    desc = "Rename",
+    unpack(bufopts)
+  })
   vim.keymap.set('n', '<space>lc', vim.lsp.buf.code_action, {
     desc = 'Code actions', unpack(bufopts)
   })
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, {
+    desc = "Get references",
+    unpack(bufopts)
+  })
   vim.keymap.set('n', '<space>lf', vim.lsp.buf.format, {
     desc = 'Format file',
     unpack(bufopts)
--- nvim/.config/nvim/lua/mattjamesdev/plugins/which-key.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugins/which-key.lua
@@ -3,7 +3,15 @@ return {
   event = 'VeryLazy',
   config = function()
     local wk = require('which-key')
-    wk.register({ f = { name = "Telescope...", }, }, { prefix = "<leader>" })
+    wk.register(
+      { b = { name = "buffer..." }, },
+      { c = { name = "CCC..." }, },
+      { f = { name = "Telescope...", }, },
+      { l = { name = "LSP..." }, },
+      { t = { name = "ToggleTerm..." }, },
+      { w = { name = "workspace..." }, },
+      { prefix = "<leader>" }
+    )
     wk.setup()
   end
 }
