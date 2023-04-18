b9cec5a2f73d8bfd08fcad59e025098ecba8ad6e
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2023-04-18 23:19:54 +1000

    fix: which-key config prefix descriptions

--- nvim/.config/nvim/lua/mattjamesdev/plugins/which-key.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugins/which-key.lua
@@ -5,10 +5,25 @@ return {
     local wk = require('which-key')
     wk.register(
       { b = { name = "buffer..." }, },
+      { prefix = "<leader>" }
+    )
+    wk.register(
       { c = { name = "CCC..." }, },
+      { prefix = "<leader>" }
+    )
+    wk.register(
       { f = { name = "Telescope...", }, },
+      { prefix = "<leader>" }
+    )
+    wk.register(
       { l = { name = "LSP..." }, },
+      { prefix = "<leader>" }
+    )
+    wk.register(
       { t = { name = "ToggleTerm..." }, },
+      { prefix = "<leader>" }
+    )
+    wk.register(
       { w = { name = "workspace..." }, },
       { prefix = "<leader>" }
     )
