9ef9e69df99beceaf021e8f6b6c9cc97869861f3
mattjamesdev <57205718+mattjamesdev@users.noreply.github.com>
2023-06-06 22:20:47 +1000

    fix: treesitter config

--- nvim/.config/nvim/lua/mattjamesdev/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mattjamesdev/plugins/treesitter.lua
@@ -1,26 +1,30 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  ensure_installed = {
-    "python",
-    "r",
-    "latex",
-    "vim",
-    "lua",
-    "bash",
-    "regex",
-    "json",
-    "javascript",
-    "html",
-    "css",
-    "typescript",
-    "tsx",
-  },
+  config = function ()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        "python",
+        "r",
+        "latex",
+        "vim",
+        "lua",
+        "bash",
+        "regex",
+        "json",
+        "javascript",
+        "html",
+        "css",
+        "typescript",
+        "tsx",
+      },
 
-  -- Syntax highlighting
-  highlight = {
-    enable = true,
-    disable = {},
-    additional_vim_regex_highlighting = false,
-  },
+      -- Syntax highlighting
+      highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
 }
